d0a96f5a7223874aa8208221edbd91a0699eaf23
Ncookiez <ncukiert@gmail.com>
2022-11-05 22:38:15 -0300

    Added Front-End on Root

---
+++ functions/static/_app/immutable/chunks/1-5c1e0582.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-03b9c5f7.js";export{t as component};
---
+++ functions/static/_app/immutable/chunks/2-fa26e638.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-6856e14f.js";export{t as component};
