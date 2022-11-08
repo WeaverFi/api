32ec49c47110dff1b30ff30e8f4f40795ad29312
Ncookiez <ncukiert@gmail.com>
2022-11-08 19:16:37 -0300

    Updated Front-End

--- functions/static/_app/immutable/chunks/1-5c1e0582.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-03b9c5f7.js";export{t as component};
---
+++ functions/static/_app/immutable/chunks/2-577e3873.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-2afe1d7e.js";export{t as component};
